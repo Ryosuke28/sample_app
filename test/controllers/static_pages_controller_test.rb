0ffd6c7281851e16c9262ee30a965b45cdfa0408
Ryosuke <roadmirage28@gmail.com>
2019-08-23 14:26:56 +0000

    finish 20190823

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,6 +10,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+  
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
 
   test "should get help" do
     get static_pages_help_url
